cdfdc27a21ee41a78e50653dee7759375982f727
heshima18 <122611275+heshima18@users.noreply.github.com>
2023-03-06 14:35:10 +0200

    Update handler.js

--- server/handler.js
+++ server/handler.js
@@ -2,16 +2,6 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let mysql = require('mysql');
 let app = express();
-let connection = mysql.createConnection({
-	host : 'localhost',
-	user : 'root',
-	password : '',
-	database : 'itzone'
-})
-connection.connect(function (error) {
-	if (error) throw error
-	console.log('connected');
-})
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
