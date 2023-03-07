07bf75588b0625718640330eb3bc308ce849de96
Raphaela Mahoro <71038695+Raphaela664@users.noreply.github.com>
2023-03-07 14:13:09 +0200

    Delete keys.js

--- auth/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    google:{
-        clientID:"58407288120-v5f1o3lakb5hum7ouofrcmu717c7hctv.apps.googleusercontent.com",
-        clientSecret:"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"
-    }
-}
