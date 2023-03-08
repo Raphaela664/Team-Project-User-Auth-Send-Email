a9eb95391bf941fd5bd541b7045953fe3433aea2
Raphaela Mahoro <71038695+Raphaela664@users.noreply.github.com>
2023-03-08 16:04:21 +0200

    Update passport-setup.js

--- auth/config/passport-setup.js
+++ auth/config/passport-setup.js
@@ -5,7 +5,7 @@ const keys = require('./keys');
 passport.use(
     new GoogleStrategy({
     //option for the srategy
-    callbackURL: 'http://team-project-auth1.onrender.com/auth/google/redirect',
+    callbackURL: 'http://team-project-auth2.onrender.com/auth/google/redirect',
     clientID:keys.google.clientID,
     clientSecret: keys.google.clientSecret
 },(accessToken,refresToken,profile,done)=>{
